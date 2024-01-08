1a707d072e3974adb5dbc3c8ae2730b7218753cf
Vinicius José <viniciusjosedev@gmail.com>
2024-01-08 20:18:10 -0300

    reset project

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import server from './app';
-import logger from './utils/logger';
-
-server.listen(process.env.PORT, () => {
-  logger.info(`Server is running on port ${process.env.PORT}`);
-});
