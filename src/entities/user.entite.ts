024460aed21de2a08d49d52ab917472b7215780d
Vinicius José <viniciusjosedev@gmail.com>
2024-01-14 15:02:09 -0300

    feat(user.entitie.ts): added more methods

--- src/entities/user.entite.ts
+++ src/entities/user.entite.ts
@@ -10,48 +10,71 @@ type UserInterface = {
 	email: string;
 	password: string;
 	token: string;
-	files?: FileInterface[];
 	storage: number;
 };
 
 export default class User {
-	private readonly _name: string;
-	private readonly _email: string;
-	private readonly _password: string;
-	private readonly _token: string;
-	private readonly _files: FileInterface[];
-	private readonly _storage: number;
+  private _name: string;
+  private _email: string;
+  private _password: string;
+  private _token: string;
+  private _storage: number;
 
-	constructor({name, email, password, token, storage, files}: UserInterface) {
-		this._name = name;
-		this._email = email;
-		this._password = password;
-		this._token = token;
-		this._storage = storage;
-		this._files = files ?? [];
-	}
+  constructor({name, email, password, token, storage}: UserInterface) {
+    this._name = name;
+    this._email = email;
+    this._password = password;
+    this._token = token;
+    this._storage = storage;
+  }
 
-	public get name(): string {
-		return this._name;
-	}
+  public get name(): string {
+    return this._name;
+  }
 
-	public get email(): string {
-		return this._email;
-	}
+  public get email(): string {
+    return this._email;
+  }
 
-	public get password(): string {
-		return this._password;
-	}
+  public get password(): string {
+    return this._password;
+  }
 
-	public get token(): string {
-		return this._token;
-	}
+  public get token(): string {
+    return this._token;
+  }
 
-	public get files(): FileInterface[] {
-		return this._files;
-	}
+  public get storage(): number {
+    return this._storage;
+  }
 
-	public get storage(): number {
-		return this._storage;
-	}
+  public update({
+    name,
+    email,
+    password,
+    storage,
+    token
+  }: Partial<UserInterface>) {
+    if (name) {
+      this._name = name;
+    } if (email) {
+      this._email = email;
+    } if (password) {
+      this._password = password;
+    } if (storage) {
+      this._storage = storage;
+    } if (token) {
+      this._token = token;
+    }
+  }
+
+  public get(): UserInterface {
+    return {
+      name: this._name,
+      email: this._email,
+      password: this._password,
+      storage: this._storage,
+      token: this._token
+    };
+  }
 }
