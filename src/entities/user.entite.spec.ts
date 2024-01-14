3e44e467f0ecd73164f23df0c8bb5d06477f1ac0
Vinicius José <viniciusjosedev@gmail.com>
2024-01-14 12:31:28 -0300

    feat(user.entitie.ts): added more getters

--- src/entities/user.entite.spec.ts
+++ src/entities/user.entite.spec.ts
@@ -1,6 +1,33 @@
+import User from './user.entite';
+
+const mockUserObj = {
+	name: 'Vinicius',
+	email: 'vinicius@vinicorp.com',
+	password: 'admin@vinicius',
+	storage: 0,
+	token: 'TOKEN',
+};
+
 describe('UserEntity', () => {
-	it('should be defined', () => {
-		const a: boolean = true;
-		expect(a).toBeTruthy();
+	it('should be instantiated', () => {
+		const user = new User(mockUserObj);
+
+		expect(user).toBeDefined();
+	});
+
+	it('should have the right attributes', () => {
+		const user = new User(mockUserObj);
+
+		expect({
+			name: user.name,
+			email: user.email,
+			password: user.password,
+			storage: user.storage,
+			token: user.token,
+			files: user.files,
+		}).toStrictEqual({
+			...mockUserObj,
+			files: [],
+		});
 	});
 });
--- src/entities/user.entite.ts
+++ src/entities/user.entite.ts
@@ -1,31 +1,57 @@
-interface FilesInterface {
+type FileInterface = {
 	name: string;
-	mimeType: string
-	type: string
-	size: number
-}
+	mimeType: string;
+	type: string;
+	size: number;
+};
 
-interface UserInterface {
-	name: string
-	email: string
-	password: string
-	token: string
-	files: FilesInterface[]
-	storage: number
-}
+type UserInterface = {
+	name: string;
+	email: string;
+	password: string;
+	token: string;
+	files?: FileInterface[];
+	storage: number;
+};
 
 export default class User {
-	private name: string
-	private email: string
-	private password: string
-	private token: string
-	private files: FilesInterface[]
-	private storage: number
+	private readonly _name: string;
+	private readonly _email: string;
+	private readonly _password: string;
+	private readonly _token: string;
+	private readonly _files: FileInterface[];
+	private readonly _storage: number;
 
-	constructor(props: UserInterface) {
-		Object.assign(this, props)
+	constructor({name, email, password, token, storage, files}: UserInterface) {
+		this._name = name;
+		this._email = email;
+		this._password = password;
+		this._token = token;
+		this._storage = storage;
+		this._files = files ?? [];
 	}
 
-			
+	public get name(): string {
+		return this._name;
+	}
 
+	public get email(): string {
+		return this._email;
+	}
+
+	public get password(): string {
+		return this._password;
+	}
+
+	public get token(): string {
+		return this._token;
+	}
+
+	public get files(): FileInterface[] {
+		return this._files;
+	}
+
+	public get storage(): number {
+		return this._storage;
+	}
 }
