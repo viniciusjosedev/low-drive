da5a280dd3384d205b64e004cce5121c264b7e09
Vinicius José <viniciusjosedev@gmail.com>
2024-01-14 15:02:24 -0300

    feat(user.entitie.spec.ts): completed test

--- src/entities/user.entite.spec.ts
+++ src/entities/user.entite.spec.ts
@@ -1,33 +1,47 @@
-import User from './user.entite';
+import User from "./user.entite";
 
 const mockUserObj = {
-	name: 'Vinicius',
-	email: 'vinicius@vinicorp.com',
-	password: 'admin@vinicius',
-	storage: 0,
-	token: 'TOKEN',
+  name: "Vinicius",
+  email: "vinicius@vinicorp.com",
+  password: "admin@vinicius",
+  storage: 0,
+  token: "TOKEN",
 };
 
-describe('UserEntity', () => {
-	it('should be instantiated', () => {
-		const user = new User(mockUserObj);
+describe("UserEntity", () => {
+  it("should be instantiated", () => {
+    const user = new User(mockUserObj);
 
-		expect(user).toBeDefined();
-	});
+    expect(user).toBeDefined();
+  });
 
-	it('should have the right attributes', () => {
-		const user = new User(mockUserObj);
+  it("should have the right attributes", () => {
+    const user = new User(mockUserObj);
 
-		expect({
-			name: user.name,
-			email: user.email,
-			password: user.password,
-			storage: user.storage,
-			token: user.token,
-			files: user.files,
-		}).toStrictEqual({
-			...mockUserObj,
-			files: [],
-		});
-	});
+    expect({
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      storage: user.storage,
+      token: user.token,
+    }).toStrictEqual(mockUserObj);
+  });
+
+  it("should have the right methods", () => {
+    const user = new User(mockUserObj);
+
+    expect(user.get()).toStrictEqual(mockUserObj);
+
+    const objUpdate = {
+      name: "test",
+      email: "test@vinicorp.com",
+      password: "admin@test",
+      storage: 10,
+      token: "ASDF",
+    };
+
+    user.update(objUpdate);
+
+    expect(user.get()).toStrictEqual(objUpdate);
+  });
 });
