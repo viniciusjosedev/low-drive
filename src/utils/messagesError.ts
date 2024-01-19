b5af320331c1d471a8257ee1b1461d4c897bdee4
Vinicius José <viniciusjosedev@gmail.com>
2024-01-19 09:20:24 -0300

    feaT(messagesError.ts): file created for export message error

---
+++ src/utils/messagesError.ts
@@ -0,0 +1,10 @@
+export default {
+  insufficientStorage: {
+    message: "Insufficient storage!",
+    statusCode: 400
+  },
+  fileNotFound: {
+    message: "File not found!",
+    statusCode: 404
+  }
+};
