f62e164d2aae369eae76e16c1394d2fad789f260
Vinicius José <viniciusjosedev@gmail.com>
2024-01-19 09:17:58 -0300

    feat(jest.config.ts): added sc/jest for transform in jest

--- jest.config.ts
+++ jest.config.ts
@@ -176,7 +176,7 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-		"^.+\\.tsx?$": "ts-jest"
+		"^.+\\.tsx?$": "@swc/jest"
 	},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
