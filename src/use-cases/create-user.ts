3b50d55d3f3a72c8e23221f7edcff2d1cc082caa
Vinicius José <viniciusjosedev@gmail.com>
2024-02-13 16:17:43 -0300

    fix: changed text of class

--- src/use-cases/create-user.ts
+++ src/use-cases/create-user.ts
@@ -1,11 +1,9 @@
 import User from "../entities/user.entity";
 import { UserInput } from "../interfaces/entities/user.entity.interface";
 import UserRepositoryImpl from "../repositories/user.repository";
-// import LowDriveError from "../errors/low-drive-error";
-// import messagesError from "../errors/messages-error";
 import { UserPersistenceOutput } from "../interfaces/entities/user.persistence.entity.interface";
 
-export default class createUserUseCase {
+export default class CreateUserUseCase {
   private _repository: UserRepositoryImpl;
 
   constructor(repository: UserRepositoryImpl) {
