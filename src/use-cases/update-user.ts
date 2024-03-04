b06d80d1c0bf9485cc97fca9bc3783e7bf98c13b
Vinicius José <viniciusjosedev@gmail.com>
2024-03-03 21:02:07 -0300

    feat: create new user case for update user

---
+++ src/use-cases/update-user.ts
@@ -0,0 +1,16 @@
+import { UserInput } from "../interfaces/entities/user.entity.interface";
+import UserRepositoryImpl from "../repositories/user.repository";
+import { UserPersistenceOutput } from "../interfaces/entities/user.persistence.entity.interface";
+
+export default class UpdateUserUseCase {
+  private _repository: UserRepositoryImpl;
+
+  constructor(repository: UserRepositoryImpl) {
+    this._repository = repository;
+  }
+
+  async execute(id: string, input: Partial<UserInput>): Promise<UserPersistenceOutput> {
+    const updateUser = this._repository.update(id, input);
+    return updateUser;
+  }
+}
